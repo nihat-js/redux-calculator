2d9facb78ab97b0a8cd7986f0766e388c951dd84
nihat-js <abdullazadenihat@gmail.com>
2023-01-11 23:27:20 +0400

    added design

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import './Index.scss'
 
 export default function Index() {
 
@@ -12,13 +13,17 @@ export default function Index() {
     <div className='home-page'>
       <div className="container">
         <div className="row">
-          <input type="number"  value={firstInputValue} onChange =  { (e) => dispatch({ value : e.target.value , type : 'setFirstInput'  })   }  />
-          <input type="number"  value={secondInputValue} onChange =  { (e) => dispatch({ value : e.target.value , type : 'setSecondInput'  })   }   />
-          <button onClick={ () => dispatch({type : 'changeOperator' ,  value :'+' })  } >  Sum   </button>
-          <button onClick={ () => dispatch( {type : 'changeOperator' ,   value :'-' })  } > Minus   </button>
-          <button onClick={ () => dispatch({ type : 'changeOperator' , value :'*' })  }  > Multiply </button>
-          <button onClick={ () => dispatch({ type : 'changeOperator' ,  value :'/' })  } > Divide   </button>
-          <p className="text"> Result is {result }  </p>
+          <div className="input-group">
+            <input type="number" value={firstInputValue} onChange={(e) => dispatch({ value: e.target.value, type: 'setFirstInput' })} />
+            <input type="number" value={secondInputValue} onChange={(e) => dispatch({ value: e.target.value, type: 'setSecondInput' })} />
+          </div>
+          <div className="actions">
+            <button onClick={() => dispatch({ type: 'changeOperator', value: '+' })} >  Sum   </button>
+            <button onClick={() => dispatch({ type: 'changeOperator', value: '-' })} > Minus   </button>
+            <button onClick={() => dispatch({ type: 'changeOperator', value: '*' })}  > Multiply </button>
+            <button onClick={() => dispatch({ type: 'changeOperator', value: '/' })} > Divide   </button>
+          </div>
+          <p className="result"> Result is {result}  </p>
         </div>
       </div>
     </div>
